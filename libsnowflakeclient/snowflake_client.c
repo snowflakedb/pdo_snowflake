422127b6e2a52546a669927e07be7d756d3b7cfa
Shige Takeda <smtakeda@gmail.com>
2017-07-28 02:34:10 +0000

    fixed comment style

--- libsnowflakeclient/snowflake_client.c
+++ libsnowflakeclient/snowflake_client.c
@@ -1,6 +1,6 @@
-//
-// Copyright (c) 2017 Snowflake Computing, Inc. All rights reserved.
-//
+/*
+ * Copyright (c) 2017 Snowflake Computing, Inc. All rights reserved.
+ */
 
 #include "snowflake_client.h"
 #include "snowflake_client_int.h"
