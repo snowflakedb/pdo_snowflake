aa33349bf1fd4d9c55c9a5022b12b1d7d0a2dc8e
Shige Takeda <smtakeda@gmail.com>
2017-12-13 19:02:52 +0000

    Consolidate PHP tests using a common.php

---
+++ tests/common.php
@@ -0,0 +1,27 @@
+<?php
+    $p = parse_ini_file(__DIR__ . "/../testenv.ini");
+
+    $account = $p['SNOWFLAKE_TEST_ACCOUNT'];
+    $user = $p['SNOWFLAKE_TEST_USER'];
+    $password = $p['SNOWFLAKE_TEST_PASSWORD'];
+    $database = $p['SNOWFLAKE_TEST_DATABASE'];
+    $schema = $p['SNOWFLAKE_TEST_SCHEMA'];
+    $warehouse = $p['SNOWFLAKE_TEST_WAREHOUSE'];
+    $role = $p['SNOWFLAKE_TEST_ROLE'];
+
+    if (array_key_exists('SNOWFLAKE_TEST_HOST', $p)) {
+        $host = $p['SNOWFLAKE_TEST_HOST'];
+    } else {
+        $host = $account . ".snowflakecomputing.com";
+    }
+    $port = "443";
+    if (array_key_exists('SNOWFLAKE_TEST_PORT', $p)) {
+        $port = $p['SNOWFLAKE_TEST_PORT'];
+    }
+    $protocol="https";
+    if (array_key_exists('SNOWFLAKE_TEST_PROTOCOL', $p)) {
+        $protocol = $p['SNOWFLAKE_TEST_PROTOCOL'];
+    }
+
+    $dsn = "snowflake:host=$host;port=$port;account=$account;database=$database;schema=$schema;warehouse=$warehouse;role=$role;protocol=$protocol";
+?>
