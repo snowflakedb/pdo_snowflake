64c65332f1a56826d0833ee623f7ba24b2327acf
Shige Takeda <smtakeda@gmail.com>
2018-01-06 06:49:43 +0000

    Update the current database, schema, warehouse and role. Validate object names

--- libsnowflakeclient/examples/connect_negative.c
+++ libsnowflakeclient/examples/connect_negative.c
@@ -64,7 +64,7 @@ int main() {
         goto cleanup;
     }
 
-    // user parameter is missing
+    // password parameter is missing
     sf = snowflake_init();
     snowflake_set_attribute(sf, SF_CON_ACCOUNT, "abc");
     snowflake_set_attribute(sf, SF_CON_USER, "abc");
@@ -81,6 +81,90 @@ int main() {
             ret = SF_STATUS_ERROR_GENERAL;
         } else {
             printf("OK. failed connection due to missing password\n");
+            ret = SF_STATUS_SUCCESS;
+        }
+    }
+    snowflake_term(sf);
+
+    // invalid database
+    sf = setup_snowflake_connection();
+    snowflake_set_attribute(sf, SF_CON_DATABASE, "NEVER_EXISTS");
+
+    ret = snowflake_connect(sf);
+    if (!ret) {
+        fprintf(stderr, "Failed. Must fail to connect\n");
+        ret = SF_STATUS_ERROR_GENERAL;
+    } else {
+        SF_ERROR *sferr = snowflake_error(sf);
+        if (!sferr->error_code == SF_STATUS_ERROR_BAD_CONNECTION_PARAMS) {
+            fprintf(stderr, "Failed. Wrong error code: %d\n",
+                    sferr->error_code);
+            ret = SF_STATUS_ERROR_GENERAL;
+        } else {
+            printf("OK. failed connection due to invalid database\n");
+            ret = SF_STATUS_SUCCESS;
+        }
+    }
+    snowflake_term(sf);
+
+    // invalid schema
+    sf = setup_snowflake_connection();
+    snowflake_set_attribute(sf, SF_CON_SCHEMA, "NEVER_EXISTS");
+
+    ret = snowflake_connect(sf);
+    if (!ret) {
+        fprintf(stderr, "Failed. Must fail to connect\n");
+        ret = SF_STATUS_ERROR_GENERAL;
+    } else {
+        SF_ERROR *sferr = snowflake_error(sf);
+        if (!sferr->error_code == SF_STATUS_ERROR_BAD_CONNECTION_PARAMS) {
+            fprintf(stderr, "Failed. Wrong error code: %d\n",
+                    sferr->error_code);
+            ret = SF_STATUS_ERROR_GENERAL;
+        } else {
+            printf("OK. failed connection due to invalid schema\n");
+            ret = SF_STATUS_SUCCESS;
+        }
+    }
+    snowflake_term(sf);
+
+    // invalid warehouse
+    sf = setup_snowflake_connection();
+    snowflake_set_attribute(sf, SF_CON_WAREHOUSE, "NEVER_EXISTS");
+
+    ret = snowflake_connect(sf);
+    if (!ret) {
+        fprintf(stderr, "Failed. Must fail to connect\n");
+        ret = SF_STATUS_ERROR_GENERAL;
+    } else {
+        SF_ERROR *sferr = snowflake_error(sf);
+        if (!sferr->error_code == SF_STATUS_ERROR_BAD_CONNECTION_PARAMS) {
+            fprintf(stderr, "Failed. Wrong error code: %d\n",
+                    sferr->error_code);
+            ret = SF_STATUS_ERROR_GENERAL;
+        } else {
+            printf("OK. failed connection due to invalid warehouse\n");
+            ret = SF_STATUS_SUCCESS;
+        }
+    }
+    snowflake_term(sf);
+
+    // invalid role
+    sf = setup_snowflake_connection();
+    snowflake_set_attribute(sf, SF_CON_ROLE, "NEVER_EXISTS");
+
+    ret = snowflake_connect(sf);
+    if (!ret) {
+        fprintf(stderr, "Failed. Must fail to connect\n");
+        ret = SF_STATUS_ERROR_GENERAL;
+    } else {
+        SF_ERROR *sferr = snowflake_error(sf);
+        if (!sferr->error_code == SF_STATUS_ERROR_BAD_CONNECTION_PARAMS) {
+            fprintf(stderr, "Failed. Wrong error code: %d\n",
+                    sferr->error_code);
+            ret = SF_STATUS_ERROR_GENERAL;
+        } else {
+            printf("OK. failed connection due to invalid role\n");
             ret = SF_STATUS_SUCCESS;
         }
     }
