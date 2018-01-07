e4af757aec81ff1c3dc3f1dc6252316b9d0d3249
Shige Takeda <smtakeda@gmail.com>
2018-01-07 04:48:30 +0000

    exit 1 if any valgrind error occurs

--- libsnowflakeclient/scripts/run_tests.sh
+++ libsnowflakeclient/scripts/run_tests.sh
@@ -12,10 +12,17 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-use_valgrind=
+VALGRIND_CMD=(
+    "valgrind"
+    "--leak-check=full"
+    "--run-libc-freeres=no"
+    "--error-exitcode=1"
+)
+
+use_valgrind=()
 while getopts "hm" opt; do
   case $opt in
-    m) use_valgrind="valgrind --leak-check=full --run-libc-freeres=no" ;;
+    m) use_valgrind=$VALGRIND_CMD ;;
     h) usage;;
     \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
     :) echo "Option -$OPTARG requires an argument." >&2; exit 1 ;;
@@ -24,5 +31,5 @@ done
 
 for f in $(ls $DIR/../cmake-build/examples/ex_*); do
     echo -e "\n==> $f"
-    $use_valgrind $f
+    ${use_valgrind[@]} $f
 done
