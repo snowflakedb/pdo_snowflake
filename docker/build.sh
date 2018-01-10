e9ad4b86edb18accde3022ad2874f3851dbd193e
Shige Takeda <smtakeda@gmail.com>
2018-01-09 17:23:08 -0800

    add php70 on ubuntu16 docker

--- docker/build.sh
+++ docker/build.sh
@@ -7,4 +7,8 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build -f $DIR/Dockerfile.ubuntu14 -t pdo-snowflake-ubuntu12:latest .
+# Ubuntu 14.04
+docker build -f $DIR/Dockerfile.ubuntu14 -t pdo-snowflake-ubuntu14 .
+
+# Ubuntu 16.04
+docker build -f $DIR/Dockerfile.ubuntu16 -t pdo-snowflake-ubuntu16 .
