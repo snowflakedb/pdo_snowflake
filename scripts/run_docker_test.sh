6a451f9b0175122c09379dcb5d985e1a876329f0
Shige Takeda <smtakeda@gmail.com>
2018-04-19 17:42:33 -0700

    Use TRAVIS_JOB_ID

--- scripts/run_docker_test.sh
+++ scripts/run_docker_test.sh
@@ -4,4 +4,9 @@
 #
 set -o pipefail
 
-docker run -v $(pwd):/cfg -v $(pwd):/base -it $DOCKER_NAME /base/docker/scripts/build_run_ubuntu.sh
+docker run \
+    -e TRAVIS_JOB_ID=$TRAVIS_JOB_ID \
+    -v $(pwd):/cfg \
+    -v $(pwd):/base \
+    -it \
+    $DOCKER_NAME /base/docker/scripts/build_run_ubuntu.sh
