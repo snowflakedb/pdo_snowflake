e0445027173b1d4c3c3c088fda957a43a7a07bb9
Shige Takeda <smtakeda@gmail.com>
2018-08-13 14:39:04 -0700

    updated proxy dockerfiles

--- docker/proxy/build.sh
+++ docker/proxy/build.sh
@@ -7,4 +7,7 @@ set -o pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
+# Squid without auth
 docker build --rm -t squid .
+# Squid with auth
+docker build -f Dockerfile.auth --rm -t squid-auth .
