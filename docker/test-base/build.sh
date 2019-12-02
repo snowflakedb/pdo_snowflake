ff9f6f6ede2e66005d0727d642d5370cce041b07
Kris Wagner <kristopher.wagner@snowflake.com>
2019-12-02 12:03:49 -0800

    Removed 7.0 and 7.1 build jobs along with associated Ubuntu images. Also updated README to reflect that we only support 7.2+

--- docker/test-base/build.sh
+++ docker/test-base/build.sh
@@ -9,12 +9,5 @@ set -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Ubuntu 14.04
-docker build -f $DIR/Dockerfile.ubuntu14 --build-arg PHP_VERSION=7.0 -t pdo-snowflake:php7.0-ubuntu14.04 .
-docker build -f $DIR/Dockerfile.ubuntu14 --build-arg PHP_VERSION=7.1 -t pdo-snowflake:php7.1-ubuntu14.04 .
-
-# Ubuntu 16.04
-docker build -f $DIR/Dockerfile.ubuntu16 -t pdo-snowflake:php7.1-ubuntu16.04 .
-
 # Ubuntu 18.04
 docker build -f $DIR/Dockerfile.ubuntu18 -t pdo-snowflake:php7.2-ubuntu18.04 .
