e0ba9a2605b0925a6996767336d9acc0fead5c68
Harry Xi <harryx@simba.com>
2021-11-01 14:04:12 -0700

    temporary try

--- scripts/build_pdo_snowflake.sh
+++ scripts/build_pdo_snowflake.sh
@@ -92,6 +92,7 @@ elif [[ "$PLATFORM" == "darwin" ]]; then
         .libs/snowflake_driver.o \
         .libs/snowflake_stmt.o \
         -Wl,-force_load,libsnowflakeclient/deps-build/darwin/arrow/lib/libarrow.a \
+        -Wl,-force_load,libsnowflakeclient/deps-build/darwin/boost/lib/libboost_system.a \
         -Wl,-force_load,libsnowflakeclient/lib/darwin/libsnowflakeclient.a \
         -Wl,-force_load,libsnowflakeclient/deps-build/darwin/openssl/lib/libcrypto.a \
         -Wl,-force_load,libsnowflakeclient/deps-build/darwin/openssl/lib/libssl.a \
