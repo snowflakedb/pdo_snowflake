96cbee41dee91f45297d7e192a98f13704b05804
sfc-gh-ext-simba-hx <hxi@magnitude.com>
2023-01-20 18:32:11 -0800

    fix build issue on git

--- scripts/build_pdo_snowflake.sh
+++ scripts/build_pdo_snowflake.sh
@@ -48,23 +48,25 @@ if [[ "$PLATFORM" == "linux" ]]; then
     echo "Linking for Linux"
     # php build delete pdo_snowflake.gcno (IDK why)
     # rebuild pdo_snowflake.c to generate it again
-    if [[ -n "$REPORT_COVERAGE" ]]; then
-    cc  -I. \
-        -I$PHP_HOME/include/php \
-        -I$PHP_HOME/include/php/main \
-        -I$PHP_HOME/include/php/TSRM \
-        -I$PHP_HOME/include/php/Zend \
-        -I$PHP_HOME/include/php/ext \
-        -I$PHP_HOME/include/php/ext/date/lib \
-        -I./libsnowflakeclient/include -DHAVE_CONFIG_H \
-        -std=c99 -Werror -g -O0 -ggdb -fprofile-arcs -ftest-coverage \
-        -DZEND_ENABLE_STATIC_TSRMLS_CACHE=1 -DZEND_COMPILE_DL_EXT=1 \
-        -c ./pdo_snowflake.c \
-        -MMD -MF pdo_snowflake.dep -MT pdo_snowflake.lo  \
-        -fPIC \
-        -DPIC \
-        -o .libs/pdo_snowflake.o
-    fi
+    # comment out since the include path doesn't work on github
+    # uncomment it manually on local build to get more accurate number
+    #if [[ -n "$REPORT_COVERAGE" ]]; then
+    #cc  -I. \
+    #    -I$PHP_HOME/include/php \
+    #    -I$PHP_HOME/include/php/main \
+    #    -I$PHP_HOME/include/php/TSRM \
+    #    -I$PHP_HOME/include/php/Zend \
+    #    -I$PHP_HOME/include/php/ext \
+    #    -I$PHP_HOME/include/php/ext/date/lib \
+    #    -I./libsnowflakeclient/include -DHAVE_CONFIG_H \
+    #    -std=c99 -Werror -g -O0 -ggdb -fprofile-arcs -ftest-coverage \
+    #    -DZEND_ENABLE_STATIC_TSRMLS_CACHE=1 -DZEND_COMPILE_DL_EXT=1 \
+    #    -c ./pdo_snowflake.c \
+    #    -MMD -MF pdo_snowflake.dep -MT pdo_snowflake.lo  \
+    #    -fPIC \
+    #    -DPIC \
+    #    -o .libs/pdo_snowflake.o
+    #fi
     g++ -shared \
         -fPIC \
         -DPIC \
