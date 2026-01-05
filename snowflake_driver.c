9675be8e9a531dd655ac297aed2812157bff577c
John Yun <jonghun.yun@insightsoftware.com>
2026-01-05 15:15:50 -0800

    add parameters for oauth authentication

--- snowflake_driver.c
+++ snowflake_driver.c
@@ -595,6 +595,13 @@ pdo_snowflake_handle_factory(pdo_dbh_t *dbh, zval *driver_options) /* {{{ */
         {"crl_memory_caching",  "true",       0},
         {"crl_disk_caching",    "true",       0},
         {"crl_download_timeout", "120",       0},
+        {"oauth_token_endpoint", NULL,      0},
+        {"oauth_authorization_endpoint", NULL, 0},
+        {"oauth_redirect_uri",  NULL,         0},
+        {"oauth_client_id",     NULL,         0},
+        {"oauth_client_secret", NULL,         0},
+        {"oauth_scope",   NULL,         0},
+        {"single_use_refresh_token", "false", 0}
     };
 
     // Parse the input data parameters
@@ -884,6 +891,35 @@ pdo_snowflake_handle_factory(pdo_dbh_t *dbh, zval *driver_options) /* {{{ */
         PDO_LOG_DBG(
             "crl_download_timeout: %d", int_attr_value);
     }
+
+    snowflake_set_attribute(H->server, SF_CON_OAUTH_AUTHORIZATION_ENDPOINT,
+        vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_AUTHORIZATION_ENDPOINT].optval);
+    PDO_LOG_DBG("oauth_authorization_endpoint: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_AUTHORIZATION_ENDPOINT].optval);
+
+    snowflake_set_attribute(H->server, SF_CON_OAUTH_TOKEN_ENDPOINT,
+        vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_TOKEN_ENDPOINT].optval);
+    PDO_LOG_DBG("oauth_token_endpoint: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_TOKEN_ENDPOINT].optval);
+
+    snowflake_set_attribute(H->server, SF_CON_OAUTH_REDIRECT_URI,
+        vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_REDIRECT_URI].optval);
+    PDO_LOG_DBG("oauth_redirect_url: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_REDIRECT_URI].optval);
+
+    snowflake_set_attribute(H->server, SF_CON_OAUTH_CLIENT_ID,
+        vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_CLIENT_ID].optval);
+    PDO_LOG_DBG("oauth_client_id: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_CLIENT_ID].optval);
+    
+    snowflake_set_attribute(H->server, SF_CON_OAUTH_CLIENT_SECRET,
+        vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_CLIENT_SECRET].optval);
+    // PDO_LOG_DBG("oauth_client_secret: %s", dbh->oauth_client_secret != NULL ? "******" : "(NULL)");
+    PDO_LOG_DBG("oauth_client_secret: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_CLIENT_SECRET].optval);
+
+    snowflake_set_attribute(H->server, SF_CON_OAUTH_SCOPE,
+        vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_SCOPE].optval);
+    PDO_LOG_DBG("oauth_scope: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_OAUTH_SCOPE].optval);
+
+    snowflake_set_attribute(H->server, SF_CON_SINGLE_USE_REFRESH_TOKEN,
+        (strcasecmp(vars[PDO_SNOWFLAKE_CONN_ATTR_SINGLE_USE_REFRESH_TOKEN].optval, "true") == 0)? &SF_BOOLEAN_TRUE :  &SF_BOOLEAN_FALSE);
+    PDO_LOG_DBG("single_use_refresh_token: %s", vars[PDO_SNOWFLAKE_CONN_ATTR_SINGLE_USE_REFRESH_TOKEN].optval);
 
     int8 ocsp_enabled = (strcasecmp(vars[PDO_SNOWFLAKE_CONN_ATTR_OCSP_DISABLE_IDX].optval, "true") != 0);
     int8 crl_enabled = (strcasecmp(vars[PDO_SNOWFLAKE_CONN_ATTR_CRL_CHECK_IDX].optval, "true") == 0);
